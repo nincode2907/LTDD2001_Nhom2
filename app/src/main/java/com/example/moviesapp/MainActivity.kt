10b4a657aa62c44630a19840c3fde9eee1d2763c
unknown <hqnga1604@gmail.com>
2023-07-17 21:41:35 +0700

    config fire base

--- app/src/main/java/com/example/moviesapp/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.example.moviesapp
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.core.content.ContextCompat
 import com.example.myapplication.screen.mainScreen.MainScreen
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +16,24 @@ class MainActivity : ComponentActivity() {
         setContent {
             window.navigationBarColor = ContextCompat.getColor(this, R.color.black)
             MainScreen()
+
+
+
+            //test fire base
+            val db = Firebase.firestore
+            val city = hashMapOf(
+                "name" to "Los Angeles",
+                "state" to "CA",
+                "country" to "USA",
+            )
+            db.collection("cities").document()
+                .set(city)
+                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+                .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
         }
     }
+
 }
 
 
